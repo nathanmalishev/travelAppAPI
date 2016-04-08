3a0f2dfc325f9c2a942f0945c63cbd58f0c26429
Nathan Malishev <nathan.malishev@gmail.com>
2016-04-08 16:18:13 +1000

    changed server listening to config files port

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var app = require('./server/server')
+var config = require('./server/config/config')
 
-var PORT = process.env.PORT || 3000;
-
-app.listen(PORT);
-console.log('Server started on PORT: '+PORT);
+app.listen(config.port);
+console.log('Server started on config.port: '+config.port);
