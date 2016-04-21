085572424a9a9390949386a27f9ad5dd7bfdf4e9
Nathan Malishev <nathan.malishev@gmail.com>
2016-04-21 16:58:22 +1000

    Group put working

--- server/API/groups/groupController.js
+++ server/API/groups/groupController.js
@@ -1,8 +1,28 @@
 var Group = require('./groupModel');
+var _ = require('lodash');
+
+var fields_to_polulate = 'users destinations.hotels.users destinations.flights.users';
+
+exports.params = function (req, res, next, id) {
+  Group.findById(id)
+    .populate(fields_to_polulate, 'username')
+    .exec()
+    .then(function (group) {
+      if (!group) {
+        next(new Error('No Group with that post'));
+      }else {
+        req.group = group;
+        next();
+      }
+    }, function (err) {
+
+      next(err);
+    });
+};
 
 exports.get = function (req, res, next) {
   Group.find()
-    .populate('users destinations.hotels.users destinations.flights.users', 'username')
+    .populate(fields_to_polulate, 'username')
     .exec()
     .then(function (groups) {
       res.json(groups);
@@ -19,9 +39,21 @@ exports.delete = function (req, res, next) {
 };
 
 exports.put = function (req, res, next) {
-  res.json({
-    username: 'nathan',
-  });
+  //TODO: if user passes in random group id they could update it even if they aren't in it
+  //need some auth
+  var group = req.group;
+
+  var update = req.body;
+
+  _.merge(group,update);
+
+  group.save(function(err,saved){
+    if(err){
+      next(err);
+    }else{
+      res.json(saved);
+    }
+  })
 };
 
 exports.post = function (req, res, next) {
--- server/API/groups/groupRoutes.js
+++ server/API/groups/groupRoutes.js
@@ -1,8 +1,13 @@
 var router = require('express').Router();
 var controller = require('./groupController');
 
+router.param('id', controller.params);
+
 router.route('/')
   .get(controller.get)
-  .post(controller.post);
+  .post(controller.post)
+
+router.route('/:id')
+  .put(controller.put);
 
 module.exports = router;
