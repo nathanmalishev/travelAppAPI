7813333a94b1aa6b05b6037c1231cab3279ba0a1
Nathan Malishev <nathan.malishev@gmail.com>
2016-04-23 08:54:08 +1000

    Give you a token when you launch the server

--- server/util/seed.js
+++ server/util/seed.js
@@ -1,6 +1,7 @@
 var User = require('../api/user/userModel');
 var Group = require('../api/group/groupModel');
 var _ = require('lodash');
+var auth = require('../api/auth/auth');
 
 var users = [
   { username: 'nathan', password: 'test', email: 'nathan@gmail.com' },
@@ -50,7 +51,7 @@ var group2 = {
 
 var cleanDB = function () {
   console.log('cleaning db');
-  var cleanPromises = [User,Group]
+  var cleanPromises = [User, Group]
     .map(function (model) {
       return model.remove().exec();
     });
@@ -88,38 +89,41 @@ var createGroups = function (data) {
     return createDoc(Group, group);
   };
 
-
   return Promise.all([
       createGroup(group1, usergroup1),
-      createGroup(group2, usergroup2)
+      createGroup(group2, usergroup2),
     ])
     .then(function (groups) {
       return _.merge({ groups: groups }, data || {});
     });
 };
 
+var addGroupToUsers = function (data) {
+  console.log('adding users to groups...');
 
-var addGroupToUsers = function(data){
-  console.log('adding users to groups...')
-
-  var updateUsers = data.groups.map(function(group){
-      return Promise.all(group.users.map(function(userId){
-        return User.findByIdAndUpdate(userId, {$push:{groups: group._id}})
-      }))
-    })
+  var updateUsers = data.groups.map(function (group) {
+      return Promise.all(group.users.map(function (userId) {
+        return User.findByIdAndUpdate(userId, { $push: { groups: group._id } });
+      }));
+    });
 
   //FIX: doesn't properly add users and their new groups to the data object
   // but it does add it properly to db
   return Promise.all(updateUsers)
-    .then(function(users){
-      return _.merge({users:users}, data || {})
-    })
+    .then(function (users) {
+      return _.merge({ users: users }, data || {});
+    });
 
-}
+};
 
 var displayData = function (data) {
-   //console.log(data);
-   console.log('Data base seeded, 6 users added and 2 groups');
+  //console.log(data);
+  console.log('Data base seeded, 6 users added and 2 groups');
+
+  //create token for users
+  console.log('***  USER TOKEN FOR ' + data.users[0].username + ' ****');
+  console.log(auth.signToken(data.users[0]._id));
+  console.log('*** END ***');
 };
 
 cleanDB()
